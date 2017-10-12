b726282c6dde5ee832972eba5aca4f32f783a061
suziebrown <suzie.brown42@gmail.com>
2017-10-12 18:46:46 +0100

    updates to forward & partialLM fns

--- forward.R
+++ forward.R
@@ -9,15 +9,22 @@
 forward <- function(X, y) {
   n <- nrow(X) #number of observations
   p <- ncol(X) #number of covariates
-  count <- 0
-  
-  A <- numeric(0) #active set
-  mu <- rep(0,p)
 
-  for (count in seq_along(p)) {
-    j <- which.max(X[-A]%*%(y-mu)) #most correlated covariate not in active set
+  A <- numeric(0) #active set
+  M <- matrix(NA, nrow=p, ncol=p) #matrix containing mu at each step
+  mu <- rep(0,p)
+  
+  if (p>n) { #LSE can't be calculated with >n covariates, return NAs
+    M[-(1:n),] <- NA 
+    warning("model parameters can only be estimated with up to n covariates, returning NA for higher-dimensional models")
+  }
+
+  for (i in seq_along(min(n,p))) {
+    M[i,] <- mu
+    j <- which.max(X[,-A]%*%(y-mu)) #most correlated covariate not in active set
     A <- c(A,j) #add j to active set
     beta <- partialLM(X,y,A) #find regression coefficients on active set
-    # ... not finished yet
+    mu <- X%*%beta
   }
+  M
 }
--- partialLM.R
+++ partialLM.R
@@ -3,7 +3,18 @@
 #' Find least squares regression coefficients using only a subset of covariates
 #' 
 #' @param A the "active set", i.e. indices of covariates that will have non-zero coefficients
+#' @param X matrix of predictor variables
+#' @param y vector of response variables
+#' 
 
-partialLM <- function(X, y, A) {
-  
+partialLM <- function(X, y, A=1:ncol(X)) {
+  if (length(A)>nrow(X)) {
+    stop("number of covariates > number of observations, can't compute least squares estimates")
+  }
+  XA <- X[,A]
+  coeff <- solve(t(XA) %*% XA) %*% (t(XA) %*% y) #calculate betas for active covariates
+  beta <- numeric(ncol(X))
+  beta[A] <- coeff
+  beta[-A] <- 0
+  beta
 }
