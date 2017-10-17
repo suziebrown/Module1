311c5b2598a8e97b3e6c96acfdda3fb2e6a74237
suziebrown <suzie.brown42@gmail.com>
2017-10-17 14:57:25 +0100

    updated print.lars to accommodate results from lasso

--- print_lars.R
+++ print_lars.R
@@ -4,6 +4,8 @@
 #' 
 #' @param x an object of class "lars"
 #' 
+#' @details Only prints betas from the best available fit for each qualitatively different model, i.e. at the last point before a new covariate is added.
+#' 
 
 print.lars <- function(x, ...) {
   B <- x$beta
@@ -21,8 +23,26 @@ print.lars <- function(x, ...) {
     print(t)
   }
   
-  if (method=="Stagewise"){
+  if (method=="Lasso") {
+    A_old <- numeric(0)
+    use <- numeric(0)
+    for (i in 1:length(t)) {
+      A_new <- which(B[,i]!=0)
+      if (length(setdiff(A_new, A_old))!=0) {
+        use <- c(use, i-1)
+      }
+      A_old <- A_new
+    }
+    use <- c(use, length(t))
     
+    cat(length(use), " models proposed \n")
+    cat("\n coefficients for each model: \n")
+    print(B[,use])
+    cat("\n L1 penalty for each model: \n")
+    print(t[use])
+  }
+  
+  if (method=="Stagewise") {
     A_old <- numeric(0)
     use <- numeric(0)
     for (i in 1:length(J)) {
